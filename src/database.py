a2030775e5bfed95cbeb1f45459cd421133b3d76
Soudagh <fr.kristina@inbox.ru>
2023-06-16 05:34:38 +0300

    some reworks

--- src/database.py
+++ src/database.py
@@ -1,12 +1,14 @@
 from typing import AsyncGenerator
 
-from sqlalchemy import MetaData, NullPool
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
-from src.config import DB_URL
+from src.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
-DATABASE_URL = DB_URL
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 Base = declarative_base()
 
 metadata = MetaData()
